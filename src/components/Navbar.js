4962949f79a47f16fecc3485c5d4a3a235e19d4e
arc-coder <harsh@arccoder.in>
2020-11-02 22:46:12 +0530

    Made Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,26 @@ import React from "react";
 const Navbar = () => {
     return (
         <div className="navbar__container">
-            <a href="#home">Arc Coder</a>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
+            <div className="left__container">
+                <a href="#home" className="logo__container">
+                    Arc Coder
+                </a>
+            </div>
+
+            <div className="right__container">
+                <a href="#home" className="home_link__container">
+                    Home
+                </a>
+                <a href="#about" className="about_link__container">
+                    About
+                </a>
+                <a href="#skills" className="skills_link__container">
+                    Skills
+                </a>
+                <a href="#projects" className="projects_link__container">
+                    Projects
+                </a>
+            </div>
         </div>
     );
 };
