1b0f0374f2c5d3b594fcf8ea003f652c224a3efa
arc-coder <harsh@arccoder.in>
2020-11-04 20:38:28 +0530

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 const Footer = () => {
-    return <div className="footer__container">Footer</div>;
+    return (
+        <div className="footer__container">
+            <div className="text__container">
+                <div className="harsh__container">
+                    <div className="data__container">
+                        Made with{" "}
+                        <span>
+                            <i className="fas fa-heart"></i>
+                        </span>{" "}
+                        and{" "}
+                        <span>
+                            <i className="fas fa-mug-hot"></i>
+                        </span>{" "}
+                        by
+                    </div>
+                    <a
+                        href="https://github.com/arc-coder"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="link__container"
+                    >
+                        Arc Coder | Harsh Patel
+                    </a>
+                </div>
+
+                <hr />
+
+                <div className="radhika__container">
+                    <div className="data__container">Logo designed by</div>
+                    <a
+                        href="https://instagram.com/fashion_illustration98"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="link__container"
+                    >
+                        Radhika Somani
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
