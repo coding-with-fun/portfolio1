dd80f8c749ddbc2280baa6ba1bd4e11848dfaf69
arc-coder <harsh@arccoder.in>
2020-11-05 17:06:13 +0530

    Home Page designed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import MyImage from "../assets/MyPhoto.jpg";
 
 const Home = () => {
-    return <div className="home__container" id="home">Home</div>;
+    return (
+        <div className="home__container" id="home">
+            <div className="left__container">
+                <div className="data__container">
+                    <div className="heading__container">
+                        Hey... <span>👋</span>
+                    </div>
+                    <div className="name__container">
+                        I am <div className="name">Harsh Patel</div>
+                    </div>
+                    <div className="details__container">
+                        A Web Developer who creates UX rich performance websites
+                        and web applications.
+                    </div>
+                </div>
+            </div>
+            <div className="right__container">
+                <img src={MyImage} alt="Logo" />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
