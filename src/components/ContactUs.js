d2bc735b77745e8459d5da655b33ca80fbb3f45a
arc-coder <harsh@arccoder.in>
2020-11-05 22:32:47 +0530

    Added Map

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { MapContainer } from "./MapContainer";
 
 const ContactUs = () => {
     return (
         <div className="contact_me__container" id="contactMe">
             Contact Us
+            <MapContainer />
         </div>
     );
 };
---
+++ src/components/MapContainer.js
@@ -0,0 +1,28 @@
+import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
+import React, { Component } from "react";
+
+const mapStyles = {
+    width: "100%",
+    height: "100%",
+};
+
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={12}
+                style={mapStyles}
+                initialCenter={{
+                    lat: 22.997666,
+                    lng: 72.60709,
+                }}
+            >
+                <Marker />
+            </Map>
+        );
+    }
+}
+MapContainer = GoogleApiWrapper({
+    apiKey: process.env.REACT_APP_API,
+})(MapContainer);
