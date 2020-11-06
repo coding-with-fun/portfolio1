6da8653eb2ef6838ee042cbad2fbe02a02106815
arc-coder <harsh@arccoder.in>
2020-11-06 11:26:44 +0530

    Tweaks

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,8 +5,40 @@ const ContactUs = () => {
     return (
         <div className="contact_me__container" id="contactMe">
             <div className="contact__heading">Contact Us</div>
-            <div className="map__container">
-                <MapContainer />
+
+            <div className="details__container">
+                <div className="my_details">
+                    <div className="my_details__heading">Email address</div>
+                    <a
+                        href="mailto:dev@arccoder.in"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="my_details__data"
+                    >
+                        dev@arccoder.in
+                    </a>
+                    <div className="my_details__heading">Contact Number</div>
+                    <a
+                        href="tel:9099976321"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="my_details__data"
+                    >
+                        90999 76321
+                    </a>
+                    <div className="my_details__heading">WhatsApp Number</div>
+                    <a
+                        href="https://wa.me/919099976321?text=Hello%20I%20am%20interested%20to%20contact%20you."
+                        target="_blank"
+                        rel="noreferrer"
+                        className="my_details__data"
+                    >
+                        90999 76321
+                    </a>
+                </div>
+                <div className="map__container">
+                    <MapContainer />
+                </div>
             </div>
         </div>
     );
