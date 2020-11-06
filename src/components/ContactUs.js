7041de9d0dfaef494515c03e0a6ad4ceb80903e2
arc-coder <harsh@arccoder.in>
2020-11-06 14:00:38 +0530

    Tweaks

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,10 +4,19 @@ import { MapContainer } from "./MapContainer";
 const ContactUs = () => {
     return (
         <div className="contact_me__container" id="contactMe">
-            <div className="contact__heading">Contact Us</div>
+            <div className="contact__heading">Get In Touch</div>
 
             <div className="details__container">
                 <div className="my_details">
+                    <div className="my_details__heading">Address</div>
+                    <a
+                        href="https://goo.gl/maps/B9ZFpSFRMkJyewpR7"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="my_details__data"
+                    >
+                        Maninagar, Ahmedabad, Gujarat 382443
+                    </a>
                     <div className="my_details__heading">Email address</div>
                     <a
                         href="mailto:dev@arccoder.in"
@@ -25,6 +34,14 @@ const ContactUs = () => {
                         className="my_details__data"
                     >
                         90999 76321
+                    </a>
+                    <a
+                        href="tel:9773195484"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="my_details__data"
+                    >
+                        97731 95484
                     </a>
                     <div className="my_details__heading">WhatsApp Number</div>
                     <a
